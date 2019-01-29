3ecd894ea78b4ba643f7bb2496d53f84ab18f443
Nguyen Le Quang <quangkeu95@gmail.com>
2019-01-29 17:25:23 +0700

    Thêm header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component, Fragment } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: "\"Open Sans\", sans-serif;",
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
+      <MuiThemeProvider theme={theme}>
         <Header />
-        <Main />
-      </Fragment>
+        {/* <Main /> */}
+      </MuiThemeProvider>
     );
   }
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,18 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import headerBackground from "./quang_sapa.jpg";
 
+import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import Hidden from "@material-ui/core/Hidden";
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
+import Typography from "@material-ui/core/Typography";
+
+import MenuIcon from "@material-ui/icons/Menu";
+import Search from "@material-ui/icons/Search";
 
 // import NavBar from "./NavBar";
 
@@ -20,12 +31,60 @@ import AppBar from "@material-ui/core/AppBar";
 
 const StyledAppBar = styled(AppBar)`
 	&& {
-		background-color: black;
+		background-color: transparent;
+		/* background-color: #DDD; */
+		box-shadow: none;
+		height: 15vh;
 	}
 `;
 
-export default class Header extends Component {
-	render() {
-		return <StyledAppBar position="fixed">a</StyledAppBar>;
+const StyledTab = styled(Tab)`
+	&& {
+		color: #000;
+		font-size: 1rem;
 	}
+`;
+
+const styles = () => ({
+	logo: {
+		fontFamily: "\"Pacifico\", cursive",
+		fontWeight: "bold"
+	}
+});
+
+function Header(props) {
+	const { classes } = props;
+
+	return (
+		<StyledAppBar position="fixed">
+			<Hidden smUp>
+				<Grid container>
+					<Grid item>
+						<IconButton>
+							<MenuIcon />
+						</IconButton>
+					</Grid>
+				</Grid>
+			</Hidden>
+			<Hidden smDown>
+				<Grid container spacing={16} justify="space-between">
+					<Grid item lg={4}>
+						<StyledTab className={classes.logo} label="Quang"></StyledTab>
+					</Grid>
+					<Grid item lg={8} >
+						<Grid container item justify="flex-end">
+							<Tabs centered>
+								<StyledTab label="Home"></StyledTab>
+								<StyledTab label="About Me"></StyledTab>
+								<StyledTab label="Portfolio"></StyledTab>
+								<StyledTab label="Contact"></StyledTab>
+							</Tabs>
+						</Grid>										
+					</Grid>
+				</Grid>
+			</Hidden>
+		</StyledAppBar>
+	)
 }
+
+export default withStyles(styles)(Header);
