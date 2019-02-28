9713d4793fa533c4b72c5a8b2382be4345f0fab8
Nguyen Le Quang <quangkeu95@gmail.com>
2019-02-28 21:23:09 +0700

    Updates

---
+++ precache-manifest.9d8207f8e4c66093fede571fad25fb3f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "939b96b64d4f8acf9ac68c1bdc0eebef",
+    "url": "/personal-website/static/media/css3-logo.939b96b6.svg"
+  },
+  {
+    "revision": "cc65b6c37ba4fcdd0c5f",
+    "url": "/personal-website/static/css/main.e52b310f.chunk.css"
+  },
+  {
+    "revision": "db9b5b2fd05d29e72f92",
+    "url": "/personal-website/static/js/1.db9b5b2f.chunk.js"
+  },
+  {
+    "revision": "7de3393924c49ef3b14b",
+    "url": "/personal-website/static/js/runtime~main.7de33939.js"
+  },
+  {
+    "revision": "1a49f8358fbaec2a859ff1d503da88f6",
+    "url": "/personal-website/static/media/quang_avatar.1a49f835.png"
+  },
+  {
+    "revision": "2241d4f6e9c6e706f07c2676b60ffd72",
+    "url": "/personal-website/static/media/rsz_quang_sapa.2241d4f6.jpg"
+  },
+  {
+    "revision": "c1cdc5beb5e84cedc57b7a216336317c",
+    "url": "/personal-website/static/media/react-logo.c1cdc5be.svg"
+  },
+  {
+    "revision": "42b540fa8bab7f4a092a5bac0675d148",
+    "url": "/personal-website/static/media/bootstrap-logo.42b540fa.svg"
+  },
+  {
+    "revision": "38ff3f4cbb2f8f3f80f902e2cb48cac4",
+    "url": "/personal-website/static/media/html5-logo.38ff3f4c.svg"
+  },
+  {
+    "revision": "cc65b6c37ba4fcdd0c5f",
+    "url": "/personal-website/static/js/main.cc65b6c3.chunk.js"
+  },
+  {
+    "revision": "dd60b11e9762b6e25f6d4ce981a1eeae",
+    "url": "/personal-website/static/media/javascript-logo.dd60b11e.svg"
+  },
+  {
+    "revision": "67a436a99d232e3f3ebc031c3967f89a",
+    "url": "/personal-website/static/media/docker-logo.67a436a9.svg"
+  },
+  {
+    "revision": "4715edc83705231bdc1d000478a4049d",
+    "url": "/personal-website/static/media/jquery-logo.4715edc8.svg"
+  },
+  {
+    "revision": "2d2a51b05422db22e02455d5de6cfa0d",
+    "url": "/personal-website/static/media/facebook.2d2a51b0.svg"
+  },
+  {
+    "revision": "a421fcee7490b8d125ce8072cc725cfe",
+    "url": "/personal-website/static/media/linkedin.a421fcee.svg"
+  },
+  {
+    "revision": "32b453001d79673940adc20878e599f0",
+    "url": "/personal-website/static/media/skype.32b45300.svg"
+  },
+  {
+    "revision": "77c43ad214e867d89f1d76eda87638dd",
+    "url": "/personal-website/static/media/github.77c43ad2.svg"
+  },
+  {
+    "revision": "5896c438beba21776a1aa36250125e09",
+    "url": "/personal-website/index.html"
+  }
+];
